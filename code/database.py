a2dd1150eeba99b863e508bb041ecc4dd8ea1d87
dung-png <badung25032012@gmail.com>
2025-08-04 23:17:34 +0700

    Initial commit

---
+++ code/database.py
@@ -0,0 +1,137 @@
+import os
+import sqlite3
+import json
+import configparser
+
+class User_DATA():
+    def __init__(self):
+        #tao folder
+        os.makedirs("database",exist_ok=True)
+
+        #country code
+        with open(r'code\countrycode.json','r') as file:
+            self.countrycode = json.load(file)
+
+        #connect database
+        self.user_db = sqlite3.connect(r"database\users.db")
+        self.user_list = []
+        self.cursor = self.user_db.cursor()
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT,business_name TEXT NOT NULL UNIQUE,username TEXT NOT NULL UNIQUE,email TEXT NOT NULL UNIQUE,password TEXT NOT NULL,phonenumber TEXT NOT NULL UNIQUE,countrycode TEXT NOT NULL)")
+        self.user_db.commit()
+        self.read()
+        for t in self.user_data:
+            print(t)
+            new_user = User(t[0],t[1],t[2],t[3],t[4],t[5],t[6])
+            self.user_list.append(new_user)
+
+    def checkkeeplogin(self):
+        if os.path.exists(r'database\config.ini'):
+            config = configparser.ConfigParser()
+            config.read(r'database\config.ini')
+            if config.has_section("Keeplogin"):
+                email = config.get("Keeplogin", "email")
+                username = config.get("Keeplogin", "username")
+                business_name = config.get("Keeplogin", "business_name")
+                password = config.get("Keeplogin", "password")
+                phonenumber = config.get("Keeplogin", "phonenumber")
+                countrycode = config.get("Keeplogin", "countrycode")
+                current_user = self.get_user_by_email(email_input=email)
+                if current_user:
+                    if username == current_user.username and business_name == current_user.business_name and password == current_user.password and phonenumber == current_user.phonenumber and countrycode == current_user.countrycode:
+                        return True
+                return False
+            
+    def read(self):
+        self.cursor.execute("SELECT * FROM users")
+        self.user_data = self.cursor.fetchall()
+    
+    def insert(self,user_tuple):
+        self.cursor.execute("INSERT INTO users (business_name,username,email,password,phonenumber,countrycode) VALUES (?,?,?,?,?,?)",user_tuple)
+        self.user_db.commit()
+    
+    def is_valid_email(self, email_input):
+        if "@" in email_input and '.' in email_input:
+            return True
+        return False
+    
+    def is_valid_password(self, password_input):
+        if len(password_input) >= 8 and len(password_input) <= 20:
+            return True
+        return False
+    
+    def is_valid_business_name(self,business_name_input):
+        if len(business_name_input) >= 3 and len(business_name_input) <= 20:
+            for user in self.user_list:
+                if user.business_name == business_name_input:
+                    return False
+            return True
+        return False
+    
+    def is_valid_username(self,username_input):
+        if len(username_input) >= 3 and len(username_input) <= 20:
+            for user in self.user_list:
+                if user.username == username_input:
+                    return False
+            return True
+        return False
+
+    def is_valid_phonenumber(self, countrycode_input, phonenumber_input):
+        list_ = self.countrycode[countrycode_input].split('.')
+        if len(phonenumber_input) >= int(list_[0]) and len(phonenumber_input) <= int(list_[1]):
+            for user in self.user_list:
+                if user.phonenumber == phonenumber_input:
+                    return False
+            return True
+        return False
+    
+    def get_user_by_email(self,email_input):
+        for user in self.user_list:
+            if user.email == email_input:
+                return user
+        return False
+    
+    def signup(self,user_tuple):
+        if not self.is_valid_password(user_tuple[3]):
+            return 1
+        if not self.is_valid_email(user_tuple[2]):
+            return 2
+        if not self.is_valid_phonenumber(user_tuple[5],user_tuple[4]): #khong trung
+            return 3
+        if not self.is_valid_business_name(user_tuple[0]): #khong trung business name
+            return 4
+        if not self.is_valid_username(user_tuple[1]): #khong trung username
+            return 5
+        if self.get_user_by_email(user_tuple[2]): #khong trung V
+            return 6
+        new_user = User(id = len(self.user_list),business_name=user_tuple[0],username=user_tuple[1],email=user_tuple[2],password=user_tuple[3],phonenumber=user_tuple[4],countrycode=user_tuple[5])
+        self.user_list.append(new_user)
+        self.insert(user_tuple)
+        return new_user
+    
+    def signin(self, user_tuple):
+        user = self.get_user_by_email(user_tuple[0].strip())
+        if user and str(user.password).strip() == str(user_tuple[1]).strip() and str(user_tuple[2]).strip() in user.phonenumber:
+            if user_tuple[3]:
+                config = configparser.ConfigParser()
+                config["Keeplogin"] = {
+                    "email": user.email,
+                    "username" : user.username,
+                    "business_name" : user.business_name,
+                    "password": user.password,
+                    "phonenumber" : user.phonenumber,
+                    "countrycode" : user.countrycode
+                    }
+                with open(r"database\config.ini", 'w') as configfile:
+                    config.write(configfile)
+            return user
+        return False
+
+class User():
+    def __init__(self,id,business_name,username,email,password,phonenumber,countrycode):
+        self.id = id
+        self.business_name = business_name.strip()
+        self.username = username.strip()
+        self.email = email.strip()
+        self.password = password.strip()
+        self.phonenumber = phonenumber.strip()
+        self.countrycode = countrycode
---
+++ code/main.py
@@ -0,0 +1,131 @@
+from PyQt6.QtWidgets import QApplication,QMainWindow,QLineEdit,QMessageBox
+from PyQt6 import uic
+import sys
+from database import User_DATA
+
+DTB = User_DATA()
+class LoginWindow(QMainWindow):
+    ui = r"ui\login_.ui"
+    def __init__(self):
+        super().__init__()
+        uic.loadUi(self.ui,self)
+        self.btn_Password.clicked.connect(self.passwordechomode)
+        self.login_btn.clicked.connect(self.Homepage)
+        self.Signup.clicked.connect(self.signup_)
+        self.keepmelogin = self.Checkbox.isChecked()
+    
+    def Homepage(self):
+        email = self.Email.text().strip()
+        password = self.Password.text().strip()
+        phonenumber = self.PhoneNumber.text().strip()
+        user_tuple = (email,password,phonenumber,self.keepmelogin)
+        if email and password:
+            resolve = DTB.signin(user_tuple)
+            if resolve:
+                self.close()
+                HomePG.show()
+            else:
+                msg_box.setText("Invalid email or password or phonenumber")
+                msg_box.exec()
+        else:
+            msg_box.setText("Please enter both email and password")
+            msg_box.exec()
+
+    def signup_(self):
+        self.close()
+        Signup.show()
+
+    def passwordechomode(self):
+        print(self.btn_Password.isChecked())
+        if not self.btn_Password.isChecked() and self.Password.echoMode() == QLineEdit.EchoMode.Password:
+            self.Password.setEchoMode(QLineEdit.EchoMode.Normal)
+        else:
+            self.Password.setEchoMode(QLineEdit.EchoMode.Password)
+
+class SignupWindow(QMainWindow):
+    ui = r"ui\signup_.ui"
+    def __init__(self):
+        super().__init__()
+        uic.loadUi(self.ui,self)
+        self.btn_Password.clicked.connect(self.passwordechomode)
+        self.signup_btn.clicked.connect(self.Homepage)
+        self.Login.clicked.connect(self.login_)
+
+    def passwordechomode(self):
+        print(self.btn_Password.isChecked())
+        if not self.btn_Password.isChecked() and self.Password.echoMode() == QLineEdit.EchoMode.Password:
+            self.Password.setEchoMode(QLineEdit.EchoMode.Normal)
+        else:
+            self.Password.setEchoMode(QLineEdit.EchoMode.Password)
+
+    def Homepage(self):
+        Business = self.Business.text().strip()
+        Username = self.Username.text().strip()
+        Email = self.Email.text().strip()
+        Password = self.Password.text().strip()
+        Confirm_Password = self.Confirm_Password.text().strip()
+        PhoneNumber = self.PhoneNumber.text().strip()
+        countrycode = self.countrycode.currentText()
+        user_tuple = (Business,Username,Email,Password,PhoneNumber,countrycode)
+        if Email and Password and Confirm_Password:
+            resolve = DTB.signup(user_tuple)
+            print(resolve)
+            if resolve == 1:
+                msg_box.setText("The password must contain between 8 and 20 characters")
+                msg_box.exec()
+            elif resolve == 2:
+                msg_box.setText("Please enter a valid email address")
+                msg_box.exec()
+            elif resolve == 3:
+                msg_box.setText("Phone number is either too short, too long, or already registered")
+                msg_box.exec()
+            elif resolve == 4:
+                msg_box.setText("Business name must be between 3 and 20 characters and must be unique")
+                msg_box.exec()
+            elif resolve == 5:
+                msg_box.setText("Username must be between 3 and 20 characters and must be unique")
+                msg_box.exec()
+            elif resolve == 6:
+                msg_box.setText("This email is already registered. Please use another one")
+                msg_box.exec()
+            else:
+                self.close()
+                HomePG.show()
+
+    def login_(self):
+        self.close()
+        Login.show()
+
+class MainWindow(QMainWindow):
+    ui = r"ui\home.ui"
+    def __init__(self):
+        super().__init__()
+        uic.loadUi(self.ui,self)
+        self.exit.clicked.connect(self.Exit)
+        self.logout.clicked.connect(self.Logout)
+    
+    def Logout(self):
+        # Ghi đè file config.ini thành file trắng (xóa hết nội dung)
+        with open(r"database\config.ini", "w") as f:
+            f.write("")  # hoặc dùng: pass
+        self.close()
+        Login.show()
+    
+    def Exit(self):
+        self.close()
+        sys.exit()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    msg_box = QMessageBox()
+    msg_box.setWindowTitle("error")
+    msg_box.setIcon(QMessageBox.Icon.Warning)
+    Login = LoginWindow()
+    Signup = SignupWindow()
+    HomePG = MainWindow()
+    keepmelogin = DTB.checkkeeplogin()
+    if keepmelogin:
+        HomePG.show()
+    else:
+        Login.show()
+    sys.exit(app.exec())
